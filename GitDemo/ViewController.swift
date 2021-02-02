eb139da872e9de3b0e24c80dca2dc80817990793
bey.ss <sergeybey17@gmail.com>
2021-02-02 14:23:02 +0700

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Сергей  Бей on 02.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
